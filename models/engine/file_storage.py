5a37c29d915b69e025229f411995e1e1ad2a617a
danielcinome <danielchinome987@gmail.com>
2020-02-14 03:32:19 +0000

    structure

---
+++ models/engine/file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    FileStorage class that serializes instances in
+    a JSON file and deserializes the JSON file
+"""
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class FileStorage(BaseModel):
+
+    def __init__(self, *args, **kwargs):
+    """
+        Constructor
+    """
+    self.__file_path
+    slef.__objects
+
+    @preperty
+    def ile_path(self):
+    """
+        Getter file_path
+    """
+	return 
+
+    @file_paths.setter
+    """
+        setter file_path
+    """
