11afa9648c3aec317670b6570f8d7a81278c93e1
Bard-Budist <op7puro@live.com>
2020-02-15 17:06:42 +0000

    Update task 07

--- console.py
+++ console.py
@@ -1,21 +1,57 @@
 #!/usr/bin/python3
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     intro = ""
     prompt = "(hbnb) "
 
-    def do_quit(self, args):
+    def do_quit(self, arg):
         "Quit command to exit the program"
         return True;
 
-    def do_EOF(self, args):
-        "Quit command to exit the program"
+    def do_EOF(self, arg):
+        "Quit command to exit the program\n"
         return True;
 
     def emptyline(self):
         return;
+
+    def do_create(self, arg):
+        "Create new instance of name class"
+        if (len(arg) == 0):
+            print("** class name missing **")
+        else:
+            try:
+                new = globals()[arg]()
+                print(new.id)
+                new.save()
+            except KeyError:
+                print("** class doesn't exist **")
+
+
+    def do_show(self, arg):
+        "Print string representation of instance"
+        if (len(arg) == 0):
+            print("** class name missing **")
+        else:
+            try:
+                command = arg.split(" ")
+                new = globals()[command[0]]()
+                if (len(command) == 1):
+                    print("** instance id missing **")
+                    return
+                obj = storage.all()
+                for key, value in obj.items():
+                    if (value.id == command[1]):
+                        print(value)
+                        return
+                print("** no instance found **")
+            except KeyError:
+                print("** class doesn't exist **")
+                
+
 
 
 if __name__ == '__main__':
