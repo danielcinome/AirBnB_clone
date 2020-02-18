b82226cb5fecc66332948e44e342e6fc28e6d369
Bard-Budist <op7puro@live.com>
2020-02-18 22:53:06 +0000

    Fix task

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""test for the BaseModel class"""
-
-
+"""Test Base Model"""
 import unittest
+import pep8
 from datetime import datetime
 import uuid
-import os
 from models.base_model import BaseModel
-import pep8
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
@@ -15,65 +12,112 @@ from models.state import State
 from models.review import Review
 
 
-class TestBaseModel(unittest.TestCase):
-    """this will test the base model class"""
+class Testpep8(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        """setup for the test"""
-        cls.base = BaseModel()
-        cls.base.name = "Kev"
-        cls.base.num = 20
+    def test_pep8_conformance_base_model(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                      "Found code style errors (and warnings).")
 
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.base
+class Testbasemodel(unittest.TestCase):
 
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
+    def test_creation_from_dictionary_basic(self):
+        """ This function proves in a basic way that when instantiating\
+            by passing a dictionary, it works correctly """
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at":
+            "2020-02-17T16:32:39.023915", "updated_at":
+            "2020-02-17T16:32:39.023940", "__class__": "BaseModel"}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name__, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date))
 
-    def test_pep8_BaseModel(self):
-        """test pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/base_model.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_creation_from_dictionary_advanced(self):
+        """ This function proves that when passing a dictionary with\
+            extra attributes, these are added correctly """
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at":
+            "2020-02-17T16:32:39.023915", "updated_at":
+            "2020-02-17T16:32:39.023940", "__class__": "BaseModel",
+            "name": "Andrea", "last_name": "Méndez"}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name__, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date))
+        self.assertEqual(my_base.name, "Andrea")
+        self.assertEqual(my_base.last_name, "Méndez")
 
-    def test_checking_for_docstring_BaseModel(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.__init__.__doc__)
-        self.assertIsNotNone(BaseModel.__str__.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def test_creation_from_dictionary_advancedx2(self):
+        """ This function proves that when passing a dictionary with\
+            extra attributes of numeric type, these are added correctly
+            and their types correspond """
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at":
+            "2020-02-17T16:32:39.023915", "updated_at":
+            "2020-02-17T16:32:39.023940", "__class__": "BaseModel", "name":
+            "Andrea", "last_name": "Méndez", "age": 20, "height": 1.68}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name__, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date))
+        self.assertEqual(my_base.name, "Andrea")
+        self.assertEqual(my_base.last_name, "Méndez")
+        self.assertEqual(my_base.age, 20)
+        self.assertEqual(my_base.height, 1.68)
+        self.assertEqual(type(my_base.age), int)
+        self.assertEqual(type(my_base.height), float)
 
-    def test_method_BaseModel(self):
-        """chekcing if Basemodel have methods"""
-        self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "save"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
+    def test_creation_from_dictionary_advancedx3(self):
+        """ This function proves that when passing a dictionary with\
+            extra attributes and with spaces in those of type string,\
+            these are added correctly """
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at":
+            "2020-02-17T16:32:39.023915", "updated_at":
+            "2020-02-17T16:32:39.023940", "__class__": "BaseModel", "name":
+            "Andrea", "last_name": "Méndez Mesias"}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name__, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date))
+        self.assertEqual(my_base.name, "Andrea")
+        self.assertEqual(my_base.last_name, "Méndez Mesias")
+        self.assertEqual(type(my_base.last_name), str)
 
-    def test_init_BaseModel(self):
-        """test if the base is an instance of type BaseModel"""
-        self.assertTrue(isinstance(self.base, BaseModel))
+    def test_isinstance(self):
+        """"Test if is an instance of the class"""
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
 
-    def test_save_BaesModel(self):
-        """test if the save method works"""
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+    def test_init(self):
+        """Test __init__
+        """
+        base = BaseModel()
+        self.assertTrue(hasattr(base, "id"))
+        self.assertTrue(hasattr(base, "created_at"))
+        self.assertTrue(hasattr(base, "updated_at"))
 
-    def test_to_dict_BaseModel(self):
-        """test if to_dictionary method works"""
-        base_dict = self.base.to_dict()
-        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(base_dict['created_at'], str)
-        self.assertIsInstance(base_dict['updated_at'], str)
+    def test_id(self):
+        """Test id
+        """
+        base0 = BaseModel()
+        base1 = BaseModel()
+        self.assertEqual(uuid.UUID(base0.id).version, 4)
+        self.assertFalse(base0.id == base1.id)
 
-if __name__ == "__main__":
-    unittest.main()
-
-
+    def test_to_dict(self):
+        """Tests the to_dict function."""
+        obj = BaseModel()
+        new_dict = obj.__dict__.copy()
+        new_dict["__class__"] = obj.__class__.__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        comparing = obj.to_dict()
+        self.assertDictEqual(new_dict, comparing)
