560926b4a225f656d62efcac81128cc9822622c3
Bard-Budist <op7puro@live.com>
2020-02-18 21:00:11 +0000

    Fix Test cases

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Test Review"""
+import unittest
+import pep8
+from models.review import Review
+from models.user import User
+from models.place import Place
+
+
+class Testreview(unittest.TestCase):
+    """TestReview"""
+    def test_pep8_conformance_review(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/review.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors.")
+
+    def test_review(self):
+        """
+        Test review
+        """
+        my_place = Place()
+        my_user = User()
+        my_review = Review()
+        my_review.place_id = my_place.id
+        my_review.user_id = my_user.id
+        my_review.text = 'holberton'
+        self.assertEqual(my_review.place_id, my_place.id)
+        self.assertEqual(my_review.user_id, my_user.id)
+        self.assertEqual(my_review.text, 'holberton')
---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Test State"""
+import unittest
+import pep8
+from models.state import State
+
+
+class Teststate(unittest.TestCase):
+    """ Test State """
+    def test_pep8_conformance_state(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors.")
+
+    def test_state(self):
+        """
+        Test attributes of Class State
+        """
+        my_state = State()
+        my_state.name = "Antioquia"
+        self.assertEqual(my_state.name, 'Antioquia')
